c6de996794f3288a33a23fe2a62af99ad70095e5
Sam Barnades <77226711+sambarnades@users.noreply.github.com>
2023-08-16 16:04:15 +0200

    Now, the task insertion function & return a correct console.log.

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
-const urlencodedParser = bodyParser.urlencoded({ extended: true })
 
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/node_modules/bootstrap/dist"));
 app.use(express.static(__dirname + "/node_modules/jquery/dist"));
@@ -17,8 +16,11 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-    console.log(req.body); // Doesn't function.
-    res.render("index.ejs")
+    let task = req.body.task;
+    res.render("index.ejs", {
+        task: task
+    })
+    console.log(task); // Doesn't function.
 });
 app.listen(port, () => {
     console.log("App listening on port " + port);
