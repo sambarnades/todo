f146d73f765d78187aa0204e1bca6720a3d20361
Sam Barnades <77226711+sambarnades@users.noreply.github.com>
2023-08-22 08:41:08 +0200

    Trying to edit the tasks array to switch between a done an undone status for the tasks realized but it doen't function. It seems that the if statement doesn't march the checkbox name attribute.

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/node_modules/bootstrap/dist"));
 app.use(express.static(__dirname + "/node_modules/jquery/dist"));
@@ -14,19 +16,35 @@ app.use(express.static(__dirname + "/node_modules/jquery/dist"));
 let tasks = [];
 
 app.get("/", (req, res) => {
-    res.render("index.ejs")
+    res.render("index.ejs", {
+        tasks: tasks,
+    })
 });
 
 app.post("/submit", (req, res) => {
 
+    if (tasks != null) {                                    // Doesn't function from this part...
+        for (let i = 0; i < tasks.length; i++) {
+
+            let checkbox = "checkbox-" + i + "-done";
+
+            if (req.body.checkbox) {
+                tasks[i].done = false;
+                console.log(tasks[i].title + " : " + tasks[i].done) 
+            } else {
+                tasks[i].done = true;
+                console.log(tasks[i].title + " : " + tasks[i].done)
+            }
+        }
+    }                                                       // To this part.
+
     class task {
         constructor() {
             this.title = req.body.title,
-            this.toDo = Date.now(),
-            this.done = false
-         }
-    } 
-    
+                this.toDo = Date.now(),
+                this.done = false
+        }
+    }
 
     let newTask = new task()
 
@@ -35,9 +53,6 @@ app.post("/submit", (req, res) => {
     res.render("index.ejs", {
         tasks: tasks,
     })
-    console.log(tasks);
-
-
 
 });
 
