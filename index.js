d0240a08495613eb9b7b962b5e7bc7b4d6c427c1
Sam B. <smauel.barnades@gmail.com>
2023-12-10 23:19:05 +0100

    Add the database & control the datetime-local module

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const ejs = require("ejs");
-const jQuery = require("jquery");
-const bodyParser = require("body-parser");
-const pg = require("pg");
-const dbPort = 5432
+import express from "express";
+import ejs from "ejs";
+import jQuery from "jquery";
+import bodyParser from "body-parser";
+import pg from 'pg';
 
-const db = new pg.Client({
-    host:"localhost",
-    port:dbPort,
-    user:"postgres",
-    password:"postgres",
-    database:"todolist"
-})
+const client = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "todolist",
+    password: "postgres",
+    port: 5432,
+});
+client.connect();
 
 const app = express();
 const port = 3000;
@@ -19,23 +19,78 @@ const port = 3000;
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.use(express.static(__dirname + "/public"));
-app.use(express.static(__dirname + "/node_modules/bootstrap/dist"));
-app.use(express.static(__dirname + "/node_modules/jquery/dist"));
+app.use(express.static("public"));
+app.use(express.static("node_modules/bootstrap/dist"));
+app.use(express.static("node_modules/jquery/dist"));
 
-let tasks = [];
+let getTasks = await client.query('SELECT * FROM tasks');
+let tasks = getTasks.rows;
+
+console.log(tasks);
+
 
 app.get("/", (req, res) => {
+
     res.render("index.ejs", {
         tasks: tasks,
     })
 });
 
-app.post("/", (req, res) => {
-    let task = req.body.task;
+app.get("/today", (req, res) => {
+
+    let sortedTasks = []; // Create a blank array
+
+    const todayLoop = () => { // Function to call the sort.
+        if (tasks) {              // Check if array exists.
+
+            tasks.forEach(taskToSort => { // Loop to check the dates.
+
+                let deadline = taskToSort.toDo;         // Stock deadline & the date today 
+                let today = new Date().toDateString();
+
+                // console.log(deadline);
+                // console.log(today);
+
+                if (deadline == today) {              // Check if the deadline is the same as the date today
+                    sortedTasks.push(taskToSort); // If it is, push it to the sortedTasks array.
+                }
+
+                else {
+
+                }
+
+            });
+            sortedTasks.forEach(task => {
+                console.log(task);
+            });
+        }
+
+        else {
+            console.log("No task array.")
+        }
+    }
+
+    todayLoop();
+
     res.render("index.ejs", {
-        tasks: tasks,
+        tasks: sortedTasks,
+        todayFirst: true
     })
+});
+
+app.post("/submit", async (req, res) => {
+
+    let toDo = new Date(req.body.toDo).toDateString();
+
+    const newTask = [
+        req.body.title,
+        toDo,
+        false
+    ]
+    
+    const query = await client.query('INSERT INTO tasks (task_name, to_do_date, done) VALUES ($1, $2, $3);', newTask);
+
+    res.redirect("/")
 
 });
 
